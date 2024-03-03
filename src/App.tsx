03f0d2e43f9e8236b0f64e1c9734a9080ac1fa02
Andrey Lebedev <alebedev424@gmail.com>
2024-03-03 19:24:56 +0300

    refactor: настроил высоту приложения

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import ProtectedRoute from './pages/ProtectedRoute/ProtectedRoute';
 import AuthPage from './pages/AuthPage/AuthPage';
+import './index.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<AuthPage />} />
-        <Route path="/" element={<ProtectedRoute />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="page">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<AuthPage />} />
+          <Route path="/" element={<ProtectedRoute />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
