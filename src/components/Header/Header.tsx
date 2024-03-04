f85b54173f1acb6a293e1bfa32bebee9c4b9e0f4
Andrey Lebedev <alebedev424@gmail.com>
2024-03-04 13:56:39 +0300

    feater: сделал header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import Avatar from '@mui/joy/Avatar';
+import Badge from '@mui/joy/Badge';
+import IconButton from '@mui/joy/IconButton';
+import AV from '../../assets/AV.png';
+import SearchIcon from '../../assets/SearchIcon.svg';
+import BadgeIcon from '../../assets/Badge_ball.svg';
+import styles from './Header.module.scss';
 
 export default function Header() {
-  return <div />;
+  return (
+    <div className={styles.header}>
+      <IconButton>
+        <img
+          className={styles.header__SearchIcon}
+          src={SearchIcon}
+          alt="Badge_icon"
+        />
+      </IconButton>
+      <div className={styles.header__badge}>
+        <Badge color="danger" size="sm" badgeContent={4}>
+          <img
+            className={styles.header__badgeIcon}
+            src={BadgeIcon}
+            alt="Badge_icon"
+          />
+        </Badge>
+      </div>
+
+      <div className={styles.header__avatar}>
+        <Avatar variant="soft" src={AV} />
+      </div>
+    </div>
+  );
 }
