f1dce627c5b1c90653a20fad79a171f7169764ef
Vsblazhev <vsblazhev@yandex.ru>
2024-03-09 16:25:52 +0100

    add profile button

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,9 +1,12 @@
 import Stack from '@mui/joy/Stack';
 import clsx from 'clsx';
+import IconButton from '@mui/joy/IconButton';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import Preferences from './preferences';
 import Info from './Info';
 import Merch from './Merch';
 import MainInfo from './MainInfo';
+
 import styles from './Profile.module.scss';
 import { useAppSelector } from '../../redux/store';
 import { openProfileSelector } from '../../redux/slices/ambassadorsSlice';
@@ -14,6 +17,13 @@ export default function Profile() {
   return (
     <div className={clsx(styles.profile, openProfile && styles.profile_opened)}>
       <div className={styles.container}>
+        <IconButton
+          className={styles.button}
+          variant="solid"
+          sx={{ backgroundColor: 'white' }}
+        >
+          <ArrowBackIosIcon sx={{ color: 'black' }} />
+        </IconButton>
         <Stack display="flex" flexDirection="row" gap="8px">
           <MainInfo />
           <Preferences />
