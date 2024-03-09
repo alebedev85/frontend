84d4f334260c1e4501ac6b9721dbfcb24f75b8eb
Andrey Lebedev <alebedev424@gmail.com>
2024-03-09 19:54:49 +0300

    feat: сделал закрытие профайла

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,28 +1,29 @@
 import Stack from '@mui/joy/Stack';
 import clsx from 'clsx';
 import IconButton from '@mui/joy/IconButton';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { ArrowForwardIos } from '@mui/icons-material';
 import Preferences from './preferences';
 import Info from './Info';
 import Merch from './Merch';
 import MainInfo from './MainInfo';
-
 import styles from './Profile.module.scss';
 import { useAppSelector } from '../../redux/store';
 import { openProfileSelector } from '../../redux/slices/ambassadorsSlice';
+import { ProfilePropsType } from './type';
 
-export default function Profile() {
+export default function Profile({ onClose }: ProfilePropsType) {
   const openProfile = useAppSelector(openProfileSelector);
 
   return (
     <div className={clsx(styles.profile, openProfile && styles.profile_opened)}>
       <div className={styles.container}>
         <IconButton
+          onClick={onClose}
           className={styles.button}
           variant="solid"
           sx={{ backgroundColor: 'white' }}
         >
-          <ArrowBackIosIcon sx={{ color: 'black' }} />
+          <ArrowForwardIos sx={{ color: 'black' }} />
         </IconButton>
         <Stack display="flex" flexDirection="row" gap="8px">
           <MainInfo />
